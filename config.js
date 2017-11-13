dca2fe87e841a5ae6afe6df2d5e9b1b1652aefee
Prider <pchomphoosang@gmail.com>
2017-11-13 22:50:04 +0700

    my chat bot

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_ID: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: '',
+    SENGRID_API_KEY: '',
+    EMAIL_FROM: '',
+    EMAIL_TO: '',
+    WEATHER_API_KEY: '',
+    ADMIN_ID: '',
+    PG_CONFIG: {
+        user: '',
+        database: '',
+        password: '',
+        host: '',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    },
+};
